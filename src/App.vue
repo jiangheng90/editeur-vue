2f6e3a70c50166442e5756716ea03405d44e1366
jiangheng90 <kaproma@qq.com>
2021-03-09 22:06:27 +0800

    演示页微调

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,52 @@
 <template>
-  <div id="app">
-    <div class="c">
-      <editeur width="600px" height="400px"></editeur>
+    <div id="app">
+        <div class="c">
+            <editeur width="600px" height="400px"></editeur>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import editeur from './components/editeur'
 export default {
-  name: 'app',
-  components: {
-    editeur
-  },
-  data () {
-    return {
-      visible: false
-    }
-  },
-  methods: {
-    afterVisibleChange(val) {
-      console.log('visible', val);
+    name: 'app',
+    components: {
+        editeur
     },
-    showDrawer() {
-      this.visible = true;
+    data() {
+        return {
+            visible: false
+        }
     },
-    onClose() {
-      this.visible = false;
+    methods: {
+        afterVisibleChange(val) {
+            console.log('visible', val)
+        },
+        showDrawer() {
+            this.visible = true
+        },
+        onClose() {
+            this.visible = false
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+html body {
+    overflow: hidden;
 }
 
 .c {
-  margin: 100px 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    margin: 100px 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
